e2e99a66cedca235f599ee969185058ca3bdc41f
Ganesh Patil <68324277+gproxx-coder@users.noreply.github.com>
2021-07-20 18:06:29 +0530

    Update number-of-1-bits.py

--- number-of-1-bits.py
+++ number-of-1-bits.py
@@ -6,3 +6,12 @@ def hammingWeight(self, n: int) -> int:
     for i in num:
         counter += int(i)
     return counter
+
+
+
+def hammingWeight(self, n: int) -> int:
+    num = map(int, bin(n)[2:])
+    counter = 0
+    for i in num:
+        counter += i
+    return counter
