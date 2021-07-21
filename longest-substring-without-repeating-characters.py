f052fec341ba5458c0a59d20749bcbace73865d5
Ganesh Patil <68324277+gproxx-coder@users.noreply.github.com>
2021-07-21 18:13:05 +0530

    Create longest-substring-without-repeating-characters.py

---
+++ longest-substring-without-repeating-characters.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/longest-substring-without-repeating-characters/submissions/
+
+"""
+Runtime: 40 ms, faster than 99.71% of Python3 online submissions for Longest Substring Without Repeating Characters.
+Memory Usage: 14.4 MB, less than 53.31% of Python3 online submissions for Longest Substring Without Repeating Characters.
+"""
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        words = set()
+        current = ''
+
+        for idx in range(len(s)):
+            if s[idx] not in current:
+                current += s[idx]
+            else:
+                if current[0] == s[idx]:
+                    current = current[1:]
+                    current += s[idx]
+                else:
+                    words.add(current)
+                    ix = current.index(s[idx])
+                    current = current[ix+1:]
+                    current += s[idx]
+        else:
+            words.add(current)
+        return len(reduce(lambda a,b: a if len(a)>len(b) else b, words))
