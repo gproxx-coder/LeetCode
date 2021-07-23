366a1fe791a04bdbffd5b5b9554df32a2a1faec2
Ganesh Patil <68324277+gproxx-coder@users.noreply.github.com>
2021-07-23 11:00:37 +0530

    Update Partition-Array-into-Disjoint-Intervals.py

--- Partition-Array-into-Disjoint-Intervals.py
+++ Partition-Array-into-Disjoint-Intervals.py
@@ -19,3 +19,24 @@ def partitionDisjoint(nums: list) -> int:
 # a = [1,1]
 a = [90,47,69,10,43,92,31,73,61,97]
 partitionDisjoint(a)
+
+# Solution 2 (Correct Solution)
+def partitionDisjoint(nums: list) -> int:
+    big, small = 0, 0
+    for idx in range(len(nums)):
+        if nums[idx] >= nums[big]:
+            big = idx
+        elif nums[idx] < nums[big]:
+            small = idx
+    else:
+        if small > big:
+            small = big - 1
+    print(small, big)
+    print(nums[:small+1], nums[small+1:])
+
+
+# a = [1,1,1,0,6,12]
+# a = [5,0,3,8,6]
+# a = [1,1]
+a = [90,47,69,10,43,92,31,73,61,97]
+partitionDisjoint(a)
